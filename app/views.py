efc971df3fadea3edca5938cbc873bc855759709
Your Name <you@example.com>
2022-07-03 17:20:01 +0500

    nodir

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from .models import *
 def bazaView(request, key):
-    fan = Fanlar.objects.get(key=key)
-    baza = Baza.objects.filter(fan=fan).last()
-    return redirect(f"{baza.baza.url}")
+    try:
+        fan = Fanlar.objects.get(key=key)
+        baza = Baza.objects.filter(fan=fan).last()
+        return redirect(f"{baza.baza.url}")
+    except :
+        return HttpResponse("Not Found")
