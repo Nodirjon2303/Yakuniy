46b5e8ceb6515bace779c18741c157fbb8f0d71b
Your Name <you@example.com>
2022-07-03 16:47:54 +0500

    nodir

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Fanlar(models.Model):
+    name = models.CharField(max_length=200)
+    key = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Baza(models.Model):
+    fan = models.ForeignKey(Fanlar, models.CASCADE)
+    baza = models.FileField(upload_to='baza savollar')
+
+    def __str__(self):
+        return f"{self.fan.name}"
+
